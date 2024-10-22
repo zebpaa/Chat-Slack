0d1ca0ccf0469e14578d23c5785a9cba7a496682
zebpaa <skvorcov_90@mail.ru>
2024-10-22 19:07:03 +0400

    add frontend/src/components/NotFoundPage.jsx

---
+++ frontend/src/components/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import { Image } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+    return (
+        <div className="text-center">
+            <Image fluid className="h-25" alt="Страница не найдена" src="src/assets/notFound.svg" />
+            <h1 className="h4 text-muted">Страница не найдена</h1>
+            <p className="text-muted">Но вы можете перейти <Link to="/">на главную страницу</Link></p>
+        </div>
+    );
+};
+
+export default NotFoundPage;
