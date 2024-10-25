6099b737c9ffe813d8ed220a9fcf487079567718
zebpaa <skvorcov_90@mail.ru>
2024-10-25 17:09:17 +0400

    update frontend/src/components/LoginPage.jsx

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,10 +1,49 @@
+import { Link } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
+import { useFormik } from 'formik';
+import axios from 'axios';
+import routes from '../routes.js';
+import { useNavigate, useLocation } from 'react-router-dom';
 import {
     Container, Row, Col, Card,
-    Form, Button, FloatingLabel, Image
+    Form, Button, FloatingLabel, Image,
 } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+
 
 const LoginPage = () => {
+    const [authFailed, setAuthFailed] = useState(false);
+    const inputRef = useRef();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        onSubmit: (values) => {
+            setAuthFailed(false);
+
+            axios.post(routes.loginPath(), values)
+                .then((data) => {
+                    console.log('data: ', data);
+                    localStorage.setItem('userId', JSON.stringify(data));
+                    const { from } = location.state;
+                    navigate(from);
+                })
+                .catch((error) => {
+                    formik.setSubmitting(false);
+                    setAuthFailed(true);
+                    console.log('error: ', error);
+                })
+
+        }
+    });
+
     return (
         <Container fluid className="h-100">
             <Row className="justify-content-center align-content-center h-100">
@@ -14,14 +53,23 @@ const LoginPage = () => {
                             <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
                                 <Image src="/images/login-avatar.jpeg" alt="Войти" roundedCircle />
                             </Col>
-                            <Form className="col-12 col-md-6 mt-3 mt-md-0">
+                            <Form className="col-12 col-md-6 mt-3 mt-md-0" onSubmit={formik.handleSubmit}>
                                 <h1 className="text-center mb-4">Войти</h1>
                                 <FloatingLabel
                                     controlId="username"
                                     label="Ваш ник"
                                     className="mb-3"
                                 >
-                                    <Form.Control placeholder="Ваш ник" name="username" autoComplete="username" required />
+                                    <Form.Control
+                                        onChange={formik.handleChange}
+                                        values={formik.values.username}
+                                        placeholder="Ваш ник"
+                                        ref={inputRef}
+                                        name="username"
+                                        autoComplete="username"
+                                        required
+                                        isInvalid={authFailed}
+                                    />
                                 </FloatingLabel>
 
                                 <FloatingLabel
@@ -29,10 +77,19 @@ const LoginPage = () => {
                                     label="Пароль"
                                     className="mb-4"
                                 >
-                                    <Form.Control type="password" placeholder="Пароль" name="password" autoComplete="current-password" required />
+                                    <Form.Control
+                                        onChange={formik.handleChange}
+                                        values={formik.values.password}
+                                        type="password"
+                                        placeholder="Пароль"
+                                        name="password"
+                                        autoComplete="current-password"
+                                        required
+                                        isInvalid={authFailed}
+                                    />
                                 </FloatingLabel>
 
-                                {/* Feedback bootstrap */}
+                                <Form.Control.Feedback type="invalid">Неверные имя пользователя или пароль</Form.Control.Feedback>
 
                                 <Button variant="outline-primary" type="submit" className="w-100 mb-3">
                                     Войти
