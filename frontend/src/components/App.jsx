4ca2fb97d35e846e020479dc80f653c9e136672e
zebpaa <skvorcov_90@mail.ru>
2024-10-28 23:53:42 +0400

    frontend/src/components/App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,17 +1,56 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar, Container } from "react-bootstrap";
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { Navbar, Container, Button } from "react-bootstrap";
 import LoginPage from "./LoginPage";
 import NotFoundPage from "./NotFoundPage";
 import SignUpPage from "./SignUpPage";
+import AuthContext from '../contexts/AuthContext';
+import useAuth from '../hooks/index.jsx';
+import HomePage from './HomePage.jsx';
+
+const AuthProvider = ({ children }) => {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const logIn = () => setLoggedIn(true);
+    const logOut = () => {
+        localStorage.removeItem('user');
+        setLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+const PrivateRoute = ({ children }) => {
+    const auth = useAuth();
+    const location = useLocation();
+
+    return (
+        auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+    );
+}
+
+const AuthButton = () => {
+    const auth = useAuth();
+
+    return (
+        auth.loggedIn && <Button onClick={auth.logOut}>Выйти</Button>
+    );
+};
 
 const App = () => {
     return (
-        <>
+        <AuthProvider>
             <div className="d-flex flex-column h-100">
                 <Router>
                     <Navbar expand="lg" className="shadow-sm bg-white">
                         <Container>
                             <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
+                            <AuthButton />
                         </Container>
                     </Navbar>
 
@@ -19,11 +58,19 @@ const App = () => {
                         <Route path="/login" element={<LoginPage />} />
                         <Route path="/signup" element={<SignUpPage />} />
                         <Route path="*" element={<NotFoundPage />} />
+                        <Route
+                            path="/"
+                            element={(
+                                <PrivateRoute>
+                                    <HomePage />
+                                </PrivateRoute>
+                            )}
+                        />
                     </Routes>
                 </Router>
             </div>
             <div className="Toastify"></div>
-        </>
+        </AuthProvider>
     );
 }
 
