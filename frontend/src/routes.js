e89e694d1a0796d74d34053cc074b84e20f7872f
zebpaa <skvorcov_90@mail.ru>
2024-10-28 14:06:54 +0400

    update frontend/src/routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,5 +2,9 @@ const apiPath = "/api/v1";
 
 export default {
   loginPath: () => [apiPath, "login"].join("/"),
-  usersPath: () => [apiPath, "data"].join("/"),
+  signupPath: () => [apiPath, "signup"].join("/"),
+  channelsPath: () => [apiPath, "channels"].join("/"),
+  channelPath: (id) => [apiPath, "channels", `:${id}`].join("/"),
+  messagesPath: () => [apiPath, "messages"].join("/"),
+  messagePath: (id) => [apiPath, "messages", `:${id}`].join("/"),
 };
