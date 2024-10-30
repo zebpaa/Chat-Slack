3fe9dd237a2869704afad92c11f5dafccc98d120
zebpaa <skvorcov_90@mail.ru>
2024-10-30 13:18:33 +0400

    update frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5002,
     proxy: {
-      // Проксируем все запросы
-      '/': {
+      // Проксируем запросы к API
+      '/api': {
         target: 'http://localhost:5001',
-        changeOrigin: true,
+      },
+      // Проксируем WebSocket соединения
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+        rewriteWsOrigin: true,
       },
     },
   },
