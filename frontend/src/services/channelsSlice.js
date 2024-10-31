5582b38f7362692634f0bebd903530f883afa4b2
zebpaa <skvorcov_90@mail.ru>
2024-10-31 13:26:58 +0400

    add frontend/src/services/channelsSlice.js

---
+++ frontend/src/services/channelsSlice.js
@@ -0,0 +1,22 @@
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+
+const channelsAdapter = createEntityAdapter();
+
+const initialState = channelsAdapter.getInitialState();
+
+const channelsSlice = createSlice({
+  name: "channels",
+  initialState,
+  reducers: {
+    addChannel: channelsAdapter.addOne,
+    addChannels: channelsAdapter.addMany,
+    removeChannel: (state, { payload }) => {
+      channelsAdapter.removeOne(state, payload);
+    },
+    updateChannel: channelsAdapter.updateOne,
+  },
+});
+
+export const { addChannel, removeChannel, updateChannel } = channelsSlice.actions;
+export const selectors = channelsAdapter.getSelectors((state) => state.channels);
+export default channelsSlice.reducer;
