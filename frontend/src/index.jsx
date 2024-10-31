8583ef20512cb1b5df044a0690074795557db261
zebpaa <skvorcov_90@mail.ru>
2024-10-31 11:27:23 +0400

    update frontend/src/index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './services/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('chat'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
