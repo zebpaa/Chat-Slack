ae501278a2d14a6f169c4f23e664e139bc841daf
zebpaa <skvorcov_90@mail.ru>
2024-11-01 15:42:02 +0400

    update frontend/src/services/index.js

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -2,9 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import messagesSlice from "./messagesSlice.js";
 import channelsSlice from "./channelsSlice.js";
+import authSlice from "./authSlice.js";
+
+// import { channelsApi } from "./channelsApi.js";
 
 const store = configureStore({
   reducer: {
+    // [channelsApi.reducerPath]: channelsApi.reducer,
+    auth: authSlice,
     channels: channelsSlice,
     messages: messagesSlice,
   },
