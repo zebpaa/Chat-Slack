59ae9da95b1a7b50bc437dc64462537947a34929
zebpaa <skvorcov_90@mail.ru>
2024-11-01 20:15:14 +0400

    update frontend/src/components/HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,16 +4,14 @@ import { Button, Container, Row, Col, Nav, Dropdown, ButtonGroup } from 'react-b
 import MessageBox from './MessageBox.jsx';
 import routes from '../routes.js';
 import { useSelector, useDispatch } from "react-redux";
-import { selectors as channelsSelectors } from '../services/channelsSlice.js'
-import { selectors as messagesSelectors } from '../services/messagesSlice.js'
-import { addChannels } from '../services/channelsSlice.js';
-import { addMessages } from '../services/messagesSlice.js';
+import { selectors as channelsSelectors, addChannels, addChannel, removeChannel } from '../services/channelsSlice.js'
+import { selectors as messagesSelectors, addMessages } from '../services/messagesSlice.js'
 
 const getAuthHeader = () => {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const token = JSON.parse(localStorage.getItem('token'));
 
-    if (user && user.token) {
-        return { Authorization: `Bearer ${user.token}` };
+    if (token) {
+        return { Authorization: `Bearer ${token}` };
     }
 
     return {};
@@ -42,6 +40,26 @@ const HomePage = () => {
         fetchMessages();
     }, []);
 
+    const handleAddChannel = () => {
+        const newChannel = { name: 'new channel' };
+        const postChannel = async () => {
+            const { data } = await axios.post(routes.channelsPath(), newChannel, { headers: getAuthHeader() });
+            dispatch(addChannel(data));
+        };
+
+        postChannel();
+    };
+
+    const handleRemoveChannel = (id) => () => {
+        const deleteChannel = async () => {
+            const { data } = await axios.delete(routes.channelPath(id), { headers: getAuthHeader() });
+            console.log('data: ', data);
+            dispatch(removeChannel(id));
+        };
+
+        deleteChannel();
+    };
+
     const renderUnremovableChannel = (channel) => (
         <Button className="w-100 rounded-0 text-start" variant="">
             <span className="me-1">#</span>{channel.name}
@@ -54,10 +72,10 @@ const HomePage = () => {
                 <span className="me-1">#</span>{channel.name}
             </Button >
             <Dropdown.Toggle className="flex-grow-0 btn" split id="dropdown-split-basic" variant="" >
-                <span class="visually-hidden">Управление каналом</span>
+                <span className="visually-hidden">Управление каналом</span>
             </Dropdown.Toggle>
             <Dropdown.Menu>
-                <Dropdown.Item as={Button} href="#">Удалить</Dropdown.Item>
+                <Dropdown.Item as={Button} href="#" onClick={handleRemoveChannel(channel.id)}>Удалить</Dropdown.Item>
                 <Dropdown.Item as={Button} href="#">Переименовать</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown >
@@ -81,7 +99,7 @@ const HomePage = () => {
                 <Col bsPrefix="col-4" className="col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
                     <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
                         <b>Каналы</b>
-                        <Button variant="group-vertical" className="p-0 text-primary">
+                        <Button variant="group-vertical" className="p-0 text-primary" onClick={handleAddChannel}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                                 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
