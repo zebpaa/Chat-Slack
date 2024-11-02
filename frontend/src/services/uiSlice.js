7391d2d221efc67fb64cb989a3bac370c24adf69
zebpaa <skvorcov_90@mail.ru>
2024-11-02 15:04:10 +0400

    add frontend/src/services/uiSlice.js

---
+++ frontend/src/services/uiSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  defaultChannelId: "1",
+  currentChannelId: "1",
+};
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    setCurrentChannel: (state, { payload }) => {
+      state.currentChannelId = payload;
+    },
+  },
+});
+
+export const { setCurrentChannel } = uiSlice.actions;
+export default uiSlice.reducer;
