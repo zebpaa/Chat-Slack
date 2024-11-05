0a42a7e6680e07444133a66fa0a85269b1a86300
zebpaa <skvorcov_90@mail.ru>
2024-11-06 01:33:29 +0400

    add frontend/src/components/modals/index.js

---
+++ frontend/src/components/modals/index.js
@@ -0,0 +1,11 @@
+import Add from "./Add";
+import Remove from "./Remove";
+import Rename from "./Rename";
+
+const modals = {
+  adding: Add,
+  removing: Remove,
+  renaming: Rename,
+};
+
+export default (modalName) => modals[modalName];
