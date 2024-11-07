33a71ad30a1b84e6894af3a4d548409d3943f900
zebpaa <skvorcov_90@mail.ru>
2024-11-07 14:51:29 +0400

    update frontend/src/components/modals/Remove.jsx

--- frontend/src/components/modals/Remove.jsx
+++ frontend/src/components/modals/Remove.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import routes from '../../routes';
 import { setCurrentChannel } from '../../services/uiSlice';
+import { useTranslation } from 'react-i18next';
 
 const getAuthHeader = () => {
     const token = JSON.parse(localStorage.getItem('token'));
@@ -13,6 +14,7 @@ const getAuthHeader = () => {
 const Remove = ({ onHide, currentChannelId, setCurrentChannelId, modalInfo: { item } }) => {
     const dispatch = useDispatch();
     const defaultChannelId = useSelector((state) => state.ui.defaultChannelId);
+    const { t } = useTranslation();
 
     const handleRemoveChannel = async () => {
         await axios.delete(routes.channelPath(item), { headers: getAuthHeader() });
@@ -25,14 +27,14 @@ const Remove = ({ onHide, currentChannelId, setCurrentChannelId, modalInfo: { it
     return (
         <Modal show>
             <Modal.Header closeButton onHide={onHide}>
-                <Modal.Title className="h4">Удалить канал</Modal.Title>
+                <Modal.Title className="h4">{t('modal.remove.heading')}</Modal.Title>
             </Modal.Header>
 
             <Modal.Body>
-                <p className="lead">Уверены?</p>
+                <p className="lead">{t('modal.remove.body')}</p>
                 <div className="d-flex justify-content-end">
-                    <Button variant="secondary" className="me-2" onClick={onHide}>Отменить</Button>
-                    <Button variant="danger" onClick={handleRemoveChannel}>Удалить</Button>
+                    <Button variant="secondary" className="me-2" onClick={onHide}>{t('modal.cancelBtn')}</Button>
+                    <Button variant="danger" onClick={handleRemoveChannel}>{t('modal.remove.submitBtn')}</Button>
                 </div>
             </Modal.Body>
         </Modal>
